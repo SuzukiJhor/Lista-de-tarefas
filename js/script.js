70fd8c9d9faa04b1f1ebf5a024dc6643a01cbe32
Suzuki Jhordan <jhordan.kendy@outlook.com>
2022-10-08 12:06:38 -0300

    finish project

--- js/script.js
+++ js/script.js
@@ -1,18 +1,11 @@
 const formTodo = document.querySelector('#todo-form')
 const formTodoInput = document.querySelector('#todo-input')
 const formSearch = document.querySelector('#search form')
-
-
 const searchInput = document.querySelector('#search-input')
-
-const todoContainer = document.querySelector('.todo-container')
-
-const editForm = document.querySelector('.edit')
+const editForm = document.querySelector('.container-edit')
 const editInput = document.querySelector('#edit-input')
 const cancelEditBtn = document.querySelector('#cancel-edit-btn')
-const searchBtn = document.querySelector('#search-button')
 const filterBtn = document.querySelector('#filter-select')
-const image = document.querySelector('.container-imagem')
 
 let oldEditInput;
 
@@ -27,10 +20,8 @@ const setTodo = event => {
     event.target.reset()
 }
 
-
 const storageUpdate = item => {
     localStorage.setItem(item, JSON.stringify(item))
-
     let keys = JSON.parse(localStorage.getItem(item))
     createTodoScreen(keys)
 }
@@ -61,22 +52,100 @@ const createTodoScreen = text => {
     document.querySelector('#todo-list').appendChild(todo)
 }
 
-
-function toggleForms() {
+const toggleForms = () => {
     editForm.classList.toggle('hide')
-    todoContainer.classList.toggle('hide')
+
 }
 
-function updateTodo(text) {
+const updateTodo = newText => {
+
     const todos = document.querySelectorAll('.todo')
     todos.forEach((todo) => {
-
         let todoTitle = todo.querySelector('h3')
 
-        if (oldEditInput == todoTitle.innerText)
-            todoTitle.innerText = text
-        localStorage.removeItem(oldEditInput)
-        storageUpdate(text)
+        if (oldEditInput.trim() == todoTitle.innerText.trim()) {
+            todoTitle.innerText = newText
+            localStorage.removeItem(oldEditInput)
+            localStorage.setItem(newText, JSON.stringify(newText))
+        }
+    })
+}
+
+const searchTask = event => {
+    event.preventDefault()
+    if (searchInput.value) {
+        const todos = document.querySelectorAll('.todo')
+        todos.forEach((item) => {
+            item.classList.add('hide')
+            if (searchInput.value.trim() == item.innerText.trim()) {
+                item.classList.remove('hide')
+            }
+        })
+    }
+    if (!searchInput.value) {
+        alert('O campo de busca esta vazio')
+    }
+}
+
+const selectClasses = event => {
+
+    const targetEl = event.target
+    const targetClass = targetEl.classList.value;
+    const parentEl = targetEl.closest('div')
+    let todoTitle;
+
+    if (parentEl && parentEl.querySelector('h3')) {
+        todoTitle = parentEl.querySelector('h3').innerText
+    }
+
+    if (targetClass == 'remove-todo') {
+        parentEl.remove()
+        localStorage.removeItem(parentEl.innerText)
+    }
+
+    if (targetClass == 'finish-todo') {
+        parentEl.classList.toggle('done')
+    }
+
+    if (targetClass == 'edit-todo') {
+        toggleForms()
+        editInput.value = todoTitle.trim()
+        oldEditInput = todoTitle.trim()
+    }
+}
+
+const editTask = event => {
+    event.preventDefault()
+    const newEditInput = editInput.value
+
+    if (newEditInput) {
+        updateTodo(newEditInput)
+    }
+    toggleForms()
+}
+
+const filterTask = event => {
+    event.preventDefault()
+    let selectValue = filterBtn.options[filterBtn.selectedIndex].value
+
+    if (selectValue === 'done') {
+        show.showDone()
+    }
+
+    if (selectValue == 'todo') {
+        show.showTodo()
+    }
+
+    if (selectValue == 'all') {
+        show.showAll()
+    }
+}
+
+const initial = () => {
+    const keys = Object.keys(localStorage)
+    keys.sort()
+    keys.forEach((item) => {
+        createTodoScreen(item)
     })
 }
 
@@ -115,93 +184,11 @@ const show = {
     }
 }
 
-const searchTask = event => {
-    event.preventDefault()
-    if (searchInput.value) {
-        const todos = document.querySelectorAll('.todo')
-        todos.forEach((item) => {
-            item.classList.add('hide')
-            if (searchInput.value.trim() == item.innerText.trim()) {
-                item.classList.remove('hide')
-            }
-        })
-    }
-    if (!searchInput.value) {
-        alert('O campo de busca esta vazio')
-    }
-}
-
-const selectClasses = event => {
-    event.preventDefault()
-    const targetEl = event.target
-    const targetClass = targetEl.classList.value;
-    const parentEl = targetEl.closest('div')
-    let todoTitle;
-
-    if (parentEl && parentEl.querySelector('h3')) {
-        todoTitle = parentEl.querySelector('h3').innerText
-
-    }
-    if (targetClass == 'remove-todo') {
-        parentEl.remove()
-        localStorage.removeItem(parentEl.innerText)
-    }
-    if (targetClass == 'finish-todo') {
-        parentEl.classList.toggle('done')
-    }
-    if (targetClass == 'edit-todo') {
-        toggleForms()
-        editInput.value = todoTitle.trim()
-        oldEditInput = todoTitle.trim()
-    }
-
-}
-
-const editTask = event => {
-    event.preventDefault()
-    const newEditInput = editInput.value
-
-    if (newEditInput) {
-        updateTodo(newEditInput)
-    }
-    toggleForms()
-}
-
-const filterTask = event => {
-    event.preventDefault()
-    let selectValue = filterBtn.options[filterBtn.selectedIndex].value
-
-    if (selectValue === 'done') {
-        show.showDone()
-    }
-
-    if (selectValue == 'todo') {
-        show.showTodo()
-    }
-
-    if (selectValue == 'all') {
-        show.showAll()
-    }
-}
-
-const initial = () => {
-    const keys = Object.keys(localStorage)
-    keys.sort()
-    keys.forEach((item) => {
-        createTodoScreen(item)
-    })
-}
-
-// document.addEventListener('click', selectClasses)
-
 formTodo.addEventListener('submit', setTodo)
-
 formSearch.addEventListener('submit', searchTask)
-
 editForm.addEventListener('submit', editTask)
-
 cancelEditBtn.addEventListener('click', toggleForms)
-
 filterBtn.addEventListener('click', filterTask)
+document.addEventListener('click', selectClasses)
 
 initial()
